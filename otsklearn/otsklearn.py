903427ed454fdefb6d71134f7a40797241cca01c
Sofiane HADDAD <sofiane_haddad@yahoo.fr>
2020-12-01 11:19:17 +0100

    Removed useless normalize data in Kriging.
    Feature has been droped from API

--- otsklearn/otsklearn.py
+++ otsklearn/otsklearn.py
@@ -123,7 +123,7 @@ class FunctionalChaos(BaseEstimator, RegressorMixin):
 class Kriging(BaseEstimator, RegressorMixin):
 
     def __init__(self, kernel='SquaredExponential', basis='Constant',
-                 n_iter_opt = 100, normalize_data = True, linalg_meth = "LAPACK"):
+                 n_iter_opt = 100, linalg_meth = "LAPACK"):
         """Kriging estimator.
 
         Parameters
@@ -134,8 +134,7 @@ class Kriging(BaseEstimator, RegressorMixin):
             Basis type
         n_iter_opt : int
             Maximal number of optimization iterations
-        normalize_data : bool
-            Tells whether input data should be normalized or not.
+            If `0` then optimization is disabled.
         linalg_meth : str
             Select the linear algebra
             Values are LAPACK or HMAT
@@ -144,7 +143,6 @@ class Kriging(BaseEstimator, RegressorMixin):
         self.kernel = kernel
         self.basis = basis
         self.n_iter_opt = n_iter_opt
-        self.normalize_data = normalize_data
         self.linalg_meth = linalg_meth
 
     def fit(self, X, y, **fit_params):
@@ -183,7 +181,7 @@ class Kriging(BaseEstimator, RegressorMixin):
         ot.ResourceMap.SetAsString(
             "KrigingAlgorithm-LinearAlgebra",  str(self.linalg_meth).upper())
         algo = ot.KrigingAlgorithm(
-            X, y, covarianceModel, basisCollection, self.normalize_data)
+            X, y, covarianceModel, basisCollection)
         if self.n_iter_opt:
             opt_algo = algo.getOptimizationAlgorithm()
             opt_algo.setMaximumIterationNumber(self.n_iter_opt)
